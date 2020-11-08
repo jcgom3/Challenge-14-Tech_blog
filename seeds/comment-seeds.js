e07293e15324fc54dc39f7e9e708a823f038a515
Juan  Gomez <jcgom3@icloud.com>
2020-11-08 03:31:57 -0500

    fixed problem on heroku with comment-seeds.js

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -16,21 +16,6 @@ const commentData = [
     post_id: 4,
     user_id: 2
   },
-  {
-    comment_text: "The asian joint at the mall is really good",
-    post_id: 4,
-    user_id: 3
-  },
-  {
-    comment_text: "There's this game call FIFA and it's pretty much a monopoly when it comes to soccer games. It sucks, but there are no other good choices so we have to stick to it",
-    post_id: 5,
-    user_id: 5
-  },
-  {
-    comment_text: "Coding can be tricky sometimes, you just have to be patient!",
-    post_id: 5,
-    user_id: 4
-  }
 ];
 
 const seedComments = () => Comment.bulkCreate(commentData);
