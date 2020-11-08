fe605095dbedb97fedac59f0fcf0617e760663ec
Juan  Gomez <jcgom3@icloud.com>
2020-11-08 17:14:18 -0500

    fixed heroku issue

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const hbs = exphbs.create({ helpers });
 
 
 const sess = {
-    secret: "Super secret secret",
+    secret: process.env.DB_SESSION_SECRET,
     cookie: { maxAge: 7200000 },
     resave: false,
     saveUninitialized: true,
