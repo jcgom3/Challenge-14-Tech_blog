b87e82f1f1230b98abdd370d5023260034cb1351
Juan  Gomez <jcgom3@icloud.com>
2020-11-08 04:10:08 -0500

    secret and extended to false

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: process.env.DB_SESSION_SECRET,
+    secret: "Super secret secret",
     cookie: { maxAge: 990000 },
     resave: false,
     saveUninitialized: true,
@@ -29,7 +29,7 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 
 
